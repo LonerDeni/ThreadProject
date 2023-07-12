f050c4721334ff00d99ffbf4fe9bbca0c09390cb
LonerDeni <124445115+LonerDeni@users.noreply.github.com>
2023-07-12 21:39:33 +0300

    Example 4 fixes v1

--- src/main/java/CollectionSynchroniz/MainCollect.java
+++ src/main/java/CollectionSynchroniz/MainCollect.java
@@ -8,13 +8,14 @@ public class MainCollect {
     static ArrayBlockingQueue<String> wordA = new ArrayBlockingQueue<>(100);
     static ArrayBlockingQueue<String> wordB = new ArrayBlockingQueue<>(100);
     static ArrayBlockingQueue<String> wordC = new ArrayBlockingQueue<>(100);
-    private static int count = 10_000;
+    private static final int count = 10_000;
+    private static final int lengthWord = 100_000;
 
     public static void main(String[] args) {
 
         new Thread(() -> {
             for (int i = 0; i < count; i++) {
-                String generateText = generateText("abc", 100_000);
+                String generateText = generateText("abc", lengthWord);
                 try {
                     wordA.put(generateText);
                     wordB.put(generateText);
@@ -26,94 +27,15 @@ public class MainCollect {
         }).start();
 
         new Thread(() -> {
-            int j = 0;
-            int maxCount = 0;
-            String maxWord = null;
-            while (j < count) {
-                if (wordA.isEmpty()) {
-                    try {
-                        Thread.sleep(100);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                for (String word : wordA) {
-                    int count = 0;
-                    for (char letter : word.toCharArray()) {
-                        if (letter == 'a')
-                            count += 1;
-                    }
-                    if (maxCount < count) {
-                        maxCount = count;
-                        maxWord = word;
-                    }
-                    try {
-                        wordA.take();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    j += 1;
-                }
-            }
-            System.out.printf("\nКоличесвто букв 'a' = %d , слово - %s", maxCount, maxWord);
+            System.out.println(theNumberChar('a', wordA));
         }).start();
 
         new Thread(() -> {
-            int j = 0;
-            long maxCount = 0;
-            String maxWord = null;
-            while (j < count) {
-                if (wordB.isEmpty()) {
-                    try {
-                        Thread.sleep(100);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                for (String word : wordB) {
-                    long countB = word.chars().filter(x -> x == 'b').count();
-                    if (maxCount < countB) {
-                        maxCount = countB;
-                        maxWord = word;
-                    }
-                    try {
-                        wordB.take();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    j += 1;
-                }
-            }
-            System.out.printf("\nКоличесвто букв 'b' = %d , слово - %s", maxCount, maxWord);
+            System.out.println(theNumberChar('b', wordB));
         }).start();
 
         new Thread(() -> {
-            int j = 0;
-            long maxCount = 0;
-            String maxWord = null;
-            while (j < count) {
-                if (wordC.isEmpty()) {
-                    try {
-                        Thread.sleep(100);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                for (String word : wordC) {
-                    long countB = word.chars().filter(x -> x == 'c').count();
-                    if (maxCount < countB) {
-                        maxCount = countB;
-                        maxWord = word;
-                    }
-                    try {
-                        wordC.take();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    j += 1;
-                }
-            }
-            System.out.printf("\nКоличесвто букв 'c' = %d , слово - %s", maxCount, maxWord);
+            System.out.println(theNumberChar('c', wordC));
         }).start();
     }
 
@@ -123,5 +45,27 @@ public class MainCollect {
             text.append(letters.charAt(random.nextInt(letters.length())));
         }
         return text.toString();
+    }
+
+    public static String theNumberChar(char letter, ArrayBlockingQueue<String> arrayWord) {
+        long maxCount = 0;
+        String maxWord = null;
+        int j = 0;
+        while (j < count){
+            for (String word : arrayWord) {
+                long count = word.chars().filter(x -> x == letter).count();
+                if (maxCount < count) {
+                    maxCount = count;
+                    maxWord = word;
+                }
+                try {
+                    arrayWord.take();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                j++;
+            }
+        }
+        return "Количесвто букв " + "'" + letter + "'" + " = " + maxCount + " , слово - " + maxWord;
     }
 }
